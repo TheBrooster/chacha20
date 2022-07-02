a0b421d35c670fb577231c4c6651c9a80210defd
Bruce McNeish <bruce@ninjakiwi.com>
2022-07-02 19:27:24 +0100

    Add extern C to the include file.

--- src/chacha20.h
+++ src/chacha20.h
@@ -2,7 +2,16 @@
 #define __CHACHA20_H
 #include <stdint.h>
 
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
 void ChaCha20XOR(uint8_t key[32], uint32_t counter, uint8_t nonce[12], uint8_t* input, uint8_t* output, int inputlen);
-void ChaCha8XOR(uint8_t key[32], uint32_t counter, uint8_t nonce[12], uint8_t *input, uint8_t *output, int inputlen);
+void ChaCha8XOR(uint8_t key[32], uint32_t counter, uint8_t nonce[12], uint8_t* input, uint8_t* output, int inputlen);
+
+
+#if defined(__cplusplus)
+}
+#endif
 
 #endif
